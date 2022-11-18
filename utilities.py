945c7b10ff61960a4ac2972be02226c5fb1d2733
Faisal Ahmed <10652987+faisalahm3d@users.noreply.github.com>
2022-11-19 01:00:49 +0600

    Matrix size is corrected

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 def get_pssm_spd(data):
   row, col = data.shape
   print (row)
-  pssm= data[:, :620]
-  spd = data[:, 620:]
-  return np.array([pssm[r,:].reshape(31,20) for r in range(row)]), np.array([spd[r, :].reshape(31,8) for r in range(row)])
+  pssm= data[:, :420]
+  spd = data[:, 420:]
+  return np.array([pssm[r,:].reshape(21,20) for r in range(row)]), np.array([spd[r, :].reshape(21,8) for r in range(row)])
